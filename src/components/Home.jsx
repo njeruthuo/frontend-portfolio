029443d77adcab1d3faf59137b06241066737a0f
njeruthuo <juliusn411@gmail.com>
2025-07-02 12:50:15 +0300

    upload fonts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,14 @@ const Home = () => {
     <section id="home" className="home-height flex items-start sm:items-center">
       <div className="w-[90%] mx-auto flex sm:flex-row flex-col-reverse place-items-center">
         <div className="sm:w-1/2 w-full">
-          <h1 className="sm:text-5xl text-xl text-center  sm:text-left mb-4">
+          <h1 className="sm:text-5xl text-xl text-center libertinus-mono-regular sm:text-left mb-4">
             Hello, I'm Julius, <br />
-            <span className="block text-gradient pt-2">
+            <span className="block libertinus-mono-regular text-gradient pt-2">
               {" "}
               a Full stack Developer
             </span>
           </h1>
-          <p className="text-white text-center text-base/6 sm:text-left pr-6">
+          <p className="text-white text-center text-base/6 sm:text-left pr-6 montserrat-500">
             Software engineer building real-world solutions — from embedded
             systems to full-stack web and mobile apps. Expert in Python
             (Django/FastAPI), C/C++ (embedded), and JavaScript/TypeScript
